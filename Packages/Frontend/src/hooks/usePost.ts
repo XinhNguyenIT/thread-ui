dbae4f3c460b01e1dc97da37d727ef095fee86dc
THLuong <110144291+HoangLuong1112@users.noreply.github.com>
2026-05-11 23:36:04 +0700

    update Comment Section

--- Packages/Frontend/src/hooks/usePost.ts
+++ Packages/Frontend/src/hooks/usePost.ts
@@ -3,26 +3,6 @@ import { getPost } from '@/api/post/fileService';
 import { PostProps } from '@/components/PostList/Post';
 import { useState, useEffect } from 'react';
 
-// export const usePosts = (page: number, pageSize: number) => {
-//     const [posts, setPosts] = useState<PostProps[]>([]);
-
-//     const fetchPosts = async () => {
-//         try {
-//             const data = await getPost({page, pageSize });
-//             console.log("data posts:", data)
-//             setPosts(data.data);
-//         } catch (err) {
-//             console.error("Post call error:", err);
-//         }
-//     };
-
-//     // Tự động gọi lại nếu page hoặc pageSize thay đổi
-//     useEffect(() => {
-//         fetchPosts();
-//     }, [page, pageSize]); 
-
-//     return { posts, refresh: fetchPosts };
-// };
 
 export const usePosts = (pageSize: number) => {
     // Mảng lưu post
